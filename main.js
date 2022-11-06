ba7bbee750e13e7f5b22b44e7c1df490c7e541b9
igor <igorvargasp@hotmail.com>
2022-11-06 19:41:06 -0300

    atualizando informações

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-document.getElementById('submit-button').addEventListener('click', e => e.preventDefault())
+const form = document.getElementById('form-tarefa');
+const tarefas = [];
 
-let inputA = document.getElementById('input-a')
-let inputB = document.getElementById('input-b')
-let button = document.getElementById('submit-button')
-let alerta = document.querySelector('.alert')
 
-button.addEventListener('click', () => {
-    if (inputA.value !== '' && inputB.value !== '') {
-        if (inputA.value >= inputB.value) {
-            alerta.innerHTML = 'A afirmação é falsa!'
-            alerta.classList.remove('yellow-alert', 'green-alert')
-            alerta.classList.add('red-alert')
-        } else {
-            alerta.innerHTML = 'A afirmação é verdadeira!'
-            alerta.classList.remove('red-alert', 'yellow-alert')
-            alerta.classList.add('green-alert')
-        }
+let linhas = '';
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+})
+
+function adicionaLinha() {
+    const inputNomeTarefa = document.getElementById('nome-tarefa');
+
+    if (tarefas.includes(inputNomeTarefa.value)) {
+        alert(`A Tarefa: ${inputNomeTarefa.value} já foi cadastrada`);
     } else {
-        alerta.innerHTML = 'Preencha os dois campos de número!'
-        alerta.classList.remove('green-alert', 'red-alert')
-        alerta.classList.add('yellow-alert')
+        tarefas.push(inputNomeTarefa.value);
+
+        let linha = '<ul>';
+        linha += `<li>${inputNomeTarefa.value}</li>`;
+        linha += `</ul>`;
+
+        linhas += linha;
     }
-});
+
+    inputNomeTarefa.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('ul');
+    corpoTabela.innerHTML = linhas;
+}
