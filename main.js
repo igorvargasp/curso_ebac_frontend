1c04b5fad8cf5be336ad3018cfbb23d653db71ec
igor <igorvargasp@hotmail.com>
2022-11-07 20:36:52 -0300

    atualizando informações

--- main.js
+++ main.js
@@ -1,44 +1,43 @@
-$(document).ready(function () {
+$('#telefone').mask('(00) 00000-0000')
 
-    $('#telefone').mask('(00) 00000-0000')
-    $('#cpf').mask('000.000.000-00')
-    $('#cep').mask('00000-000')
+$('#cpf').mask('000.000.000-00')
 
-    $('form').validate({
-        rules: {
-            nome: {
-                required: true
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            telefone: {
-                required: true
-            },
-            cpf: {
-                required: true
-            },
-            cep: {
-                required: true
-            },
+$('#cep').mask('00000-000')
+
+$('form').validate({
+    rules: {
+        nome: {
+            required: true
         },
-
-        messages: {
-            nome: 'Por favor, insira o seu nome',
-            telefone: 'Por favor, forneça um telefone válido.',
-            cpf: 'Por favor, forneça um CPF válido.',
-            cep: 'Por favor, forneça um CEP válido.',
+        cpf: {
+            required: true
         },
-
-        submitHandler: function (form) {
-            console.log(form)
+        email: {
+            required: true,
+            email: true
         },
-        invalidHandler: function (evento, validador) {
-            let camposIncorretos = validador.numberOfInvalids();
-            if (camposIncorretos) {
-                alert(`Existem ${camposIncorretos} campos incorretos`)
-            }
+        telefone: {
+            required: true
+        },
+        endereco: {
+            required: true,
+        },
+        cep: {
+            required: true,
         }
-    })
+
+    },
+    messages: {
+        nome: 'Por favor, insira seu nome completo.'
+    },
+    submitHandler: function (form) {
+        console.log(form)
+    },
+    invalidHandler: function (evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos.`)
+        }
+    }
+
 })
